38675311c567b6452e3d7358e52f801d6bdb8329
BetriuJaume <betriutortjaume@gmail.com>
2023-09-09 15:23:38 +0200

    new chatbot with memory

--- memory_chat.py
+++ memory_chat.py
@@ -3,6 +3,7 @@ import sys
 import streamlit as st
 from PIL import Image
 from utils.functions import is_negative_response
+import time
 
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
@@ -10,28 +11,39 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
-from htmlTemplates import css, bot_template, user_template
+# Tutorial langchain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
 # llm = OpenAI(openai_api_key="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")
 # Personal: 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
 # Connecthink: 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
 
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm = llm,
-        retriever=vectorstore,
-        memory=memory
-    )
+def show_chat_history() -> None:
 
-    return conversation_chain
+    # If there is no chat history:
+    if len(st.session_state.chat_history) == 0:
+        pass
+    else:
+        # Iterate trough the messages in the chat history and show them in html format:
+        for question_answer in st.session_state.chat_history:
+            with st.chat_message("user"):
+                st.write(question_answer[0])
+            with st.chat_message("assistant"):
+                st.write(question_answer[1])
 
+# Define the chat history:
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
-st.write(css, unsafe_allow_html=True)
-
-if "conversation" not in st.session_state:
-    st.session_state.conversation = None
+# Define if is is the first time the user enters the sessin or not:
+if "first_refresh_session" not in st.session_state:
+    st.session_state.first_refresh_session = True
+else:
+    st.session_state.first_refresh_session = False
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
@@ -42,57 +54,46 @@ gpt_logo = Image.open("images/Chat_gpt_logo.png")
 
 st.image(dqs_logo)
 
-mode_radio = st.radio(
-    "Por favor, selecciona el tema en el que necesitas que te asesore:",
-    [
-        "Quiero conocer DQS y explorar los servicios que ofrecen sin entrar en detalle",
-        "Estoy interesado en el paquete de aplicaciones Dynamics 365",
-        "Estoy interesado en conocer los servicios que ofrece DQS en el ámbito de la Inteligencia Artificial"
-    ]
-)
+# Do the embedings and everything only if 
+if st.session_state.first_refresh_session:
+    with st.spinner("Cargando chatbot..."):
+        loader = TextLoader("chatbot_databases/general_data.txt")
+        documents = loader.load()
 
-modes_dict = {
-    "Quiero conocer DQS y explorar los servicios que ofrecen sin entrar en detalle": ("general", "Puedes preguntarme lo que quieras de nuestra empresa"),
-    "Estoy interesado en el paquete de aplicaciones Dynamics 365": ("dynamics", "Puedes preguntarme lo que quieras sobre el paquete de aplicaciones Dynamics 365"),
-    "Estoy interesado en conocer los servicios que ofrece DQS en el ámbito de la Inteligencia Artificial": ("IA", "Puedes preguntarme lo que quieras sobre servicios que ofrece DQS en el ámbito de la Inteligencia Artificial")
-}
+        text_splitter = CharacterTextSplitter() # chunk_size=1000, chunk_overlap=200
+        documents = text_splitter.split_documents(documents)
 
-mode, text_input_label = modes_dict[mode_radio]
+        embeddings = OpenAIEmbeddings()
+        vectorstore = Chroma.from_documents(documents, embeddings)
 
-with st.form(key="text_input"):
-    query = st.text_input(f"👋 Hola! Soy el Chat-Bot de DQS. {text_input_label}")
-    
-    extense = st.checkbox("Genera una respuesta extensa (Comporta tiempos de carga ligeramente superiores)")
-    
-    #query = sys.argv[1]
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-    submit_button_clicked = st.form_submit_button(label="Submit")
+        # Store the qa in session state so it can be always accessed:
+        st.session_state.qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model="gpt-3.5-turbo-16k"), vectorstore.as_retriever(), memory=memory)
 
-if submit_button_clicked:
-    with st.spinner("Preparando la mejor respuesta para tu pregunta..."):
+show_chat_history()
 
-        loader = TextLoader(f"chatbot_databases/{mode}_data.txt")
-        index = VectorstoreIndexCreator().from_loaders([loader])
+query = st.chat_input("Escribe tu consulta")
 
-        st.session_state.conversation = get_conversation_chain(index)
-
-        st.write(user_template.replace("{{MSG}}", "hello robot"), unsafe_allow_html=True)
-        st.write(bot_template.replace("{{MSG}}", "hello human"), unsafe_allow_html=True)
-
-
+if query:
+    with st.spinner("Generando respuesta..."):
         
-#         if extense:
-#             query = query + " Necessito que me des una respuesta extensa."
+        # Get only th etwo lasts question and answers from the chat_history to avoid huge costs:
+        context = st.session_state.chat_history[-2:]
 
-#         # query = "Contesta la siguiente petición solo si estas al 100% seguro de que es cierta. Si no estas seguro contesta que no lo sabes:" + query
+        # Generate the answer:
+        result = st.session_state.qa({"question": query, "chat_history": context})
 
-#         response = index.query(query, llm=ChatOpenAI())
+        # Show the question:
+        with st.chat_message("user"):
+            st.write(result["question"])
+        # Show the answer:
+        with st.chat_message("assistant"):
+            st.write(result["answer"])
+        
+        # Add the queries to the chat_history
+        st.session_state.chat_history.append((query, result["answer"]))
 
-#         if is_negative_response(response):
-#             st.warning("No dispongo de la información para responder a tu pregunta. Para una informacion más detallada visita la web https://www.dqsconsulting.com/dqsconsulting/ o contacta con nosotros rellenando el formulario https://www.dqsconsulting.com/contacto/")
-#         else:
-#             st.info(response)
-
-# with st.columns(2)[1]:
-#     st.write("Powered by:")
-#     st.image(gpt_logo, width=250)
+with st.columns(2)[1]:
+    st.write("Powered by:")
+    st.image(gpt_logo, width=250)
