3fce1a50160cf106b1a9570648192840ff2d24e1
BetriuJaume <betriutortjaume@gmail.com>
2023-09-09 16:35:30 +0200

    try to fixt the sqlite problem

--- memory_chat.py
+++ memory_chat.py
@@ -22,6 +22,11 @@ from langchain.memory import ConversationBufferMemory
 # Personal: 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
 # Connecthink: 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
 
+# Streamlit cloud has some problems with the version of sqlite3. So we are adding to the requirements the package pysqlite3-binary and using them:
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 def show_chat_history() -> None:
 
     # If there is no chat history:
